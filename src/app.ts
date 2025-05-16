678337a67fea722310987e712609e75362c0b206
PauloMats <pm.mats98@gmail.com>
2025-05-16 10:53:42 -0300

    feat: app and server basic structure and routing // Description: This code sets up a basic Express server with routing for a React app. // It serves static files from the 'build' directory and handles API requests. // The server listens on port 3000 or the port specified in the environment variables.

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import notaFiscalRoutes from './routes/notaFiscal.routes';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json';
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api/notas-fiscais', notaFiscalRoutes);
+
+export default app;
